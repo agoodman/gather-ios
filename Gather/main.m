6036a57d88e7a615993c4c9e310829bc21c79a56
Aubrey <aubrey.goodman@gmail.com>
2013-04-29 16:54:41 -0400

    Initial Commit

---
+++ Gather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gather
+//
+//  Created by Aubrey Goodman on 4/29/13.
+//  Copyright (c) 2013 Migrant Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
